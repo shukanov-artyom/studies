a5f3ccef694e71dee716ba4e46500c4968f402a5
Artyom <shukanov.artyom@gmail.com>
2015-12-29 18:44:49 +0300

    Serilog excercises

---
+++ .NET/Serilog/helloworld/helloworld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using Serilog;
+
+namespace helloworld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // configure log4net in app.config
+            XmlConfigurator.Configure();
+            ILog log = LogManager.GetLogger("helloworld.logger");
+            log.Error("Some Error!");
+
+            // configure Serilog to write to console and to log4net sink
+            LoggerConfiguration config = new LoggerConfiguration();
+            config.WriteTo.ColoredConsole();
+            config.WriteTo.Log4Net(defaultLoggerName: "helloworld.logger");
+            ILogger logger = config.CreateLogger();
+
+            logger.Information("Some data from Serilog!");
+            logger.Error("Some error from Serilog!");
+
+            Console.Read();
+        }
+    }
+}
