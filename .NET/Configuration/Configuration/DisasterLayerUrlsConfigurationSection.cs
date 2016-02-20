c070b4758eadbc8ec370a293cf86c6635a6f2225
Artyom <a.shukanov@itransition.com>
2016-02-20 20:45:17 +0300

    wpf and gmaps excercise

---
+++ .NET/Configuration/Configuration/DisasterLayerUrlsConfigurationSection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+
+namespace Configuration
+{
+    /// <summary>
+    /// Configuration section for configuring required Disaster Map layers.
+    /// </summary>
+    public class DisasterLayerUrlsConfigurationSection : ConfigurationSection
+    {
+        private static readonly ConfigurationProperty UrlsCollectionProperty;
+        private static readonly ConfigurationProperty NameProperty;
+        private static readonly ConfigurationPropertyCollection properties;
+
+        /// <summary>
+        /// Initializes <see cref="DisasterLayerUrlsConfigurationSection"/> class.
+        /// </summary>
+        static DisasterLayerUrlsConfigurationSection()
+        {
+            UrlsCollectionProperty = new ConfigurationProperty(
+                String.Empty,
+                typeof (DisasterMapUrlsCollection),
+                null,
+                ConfigurationPropertyOptions.IsRequired | ConfigurationPropertyOptions.IsDefaultCollection
+                );
+            properties = new ConfigurationPropertyCollection();
+            properties.Add(UrlsCollectionProperty);
+        }
+
+        /// <summary>
+        /// Overrides Properties collection.
+        /// </summary>
+        protected override ConfigurationPropertyCollection Properties
+        {
+            get
+            {
+                return properties;
+            }
+        }
+
+        /// <summary>
+        /// Exposes configured Urls.
+        /// </summary>
+        public DisasterMapUrlsCollection Urls
+        {
+            get
+            {
+                return (DisasterMapUrlsCollection)base[UrlsCollectionProperty];
+            }
+        }
+    }
+}
---
+++ .NET/Google Maps/maps/mapsForms/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System;
+using System.Windows.Forms;
+
+namespace mapsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchClick(object sender, EventArgs e)
+        {
+            string city = textBoxCity.Text;
+            string query = String.Format("http://maps.google.com/maps?q={0}", city);
+            webBrowser1.Navigate(query);
+        }
+    }
+}
