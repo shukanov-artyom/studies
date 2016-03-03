36df3a359222b0b116ee803eabaae758d827ba47
Artyom <a.shukanov@itransition.com>
2016-03-03 16:51:19 +0300

    dev

---
+++ JavaScript/helloworld/hw.js
@@ -0,0 +1,13 @@
+'use strict'
+function clicked() {
+    var message = "button clicked."
+    alert(message);
+    test();
+}
+
+function test() {
+    let a = '3';
+    let b = 4;
+    let d = a + b;
+    console.log(d); 
+}
