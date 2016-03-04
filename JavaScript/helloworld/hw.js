9605461e2cf630974db0b63b2f87744b0b161ad8
Artyom <a.shukanov@itransition.com>
2016-03-04 15:50:58 +0300

    experiments with prototyping n javascript

--- JavaScript/helloworld/hw.js
+++ JavaScript/helloworld/hw.js
@@ -1,8 +1,44 @@
 'use strict'
+
 function clicked() {
     var message = "button clicked."
     alert(message);
     test();
+}
+
+function clickedProto() {
+    
+    // create dog with functions
+    function getAnimal(name, age){
+        return {
+            name: name,
+            age: age,
+            talk: function(){
+                alert(this.name + ' ' + 'makes noise');
+            }
+        }
+    }
+    
+    var animal = getAnimal('Rex', 2);
+    console.log(animal); 
+    
+    // create dog with prototyping
+    function AnotherAnimal(name, age){
+        this.name = name,
+        this.age = age
+    }
+    AnotherAnimal.prototype = {
+        talk: function(){
+            console.log('Another animal is barking because it is a dog!');
+        }
+    }
+    
+    var dog = new AnotherAnimal('Pete', 3);
+    console.log(dog);
+}
+
+function clickedSimpleProto() {
+    
 }
 
 function test() {
