1c3a85ab62fee386ae5440213d08861e3fe0cbb6
Shukanov <a.shukanov@itransition.com>
2016-08-17 17:14:26 +0300

    Certification preparation

---
+++ .NET/Certification/1. Multithreading/Multithreading/TaskTests/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Func<int> work = () =>
+            {
+                Console.WriteLine("Delegate called!");
+                return 0;
+            }; // просто делегат нужной сигнатуры
+
+            Task<int> task = new Task<int>(work);
+            task.Wait();*/
+            //TestTask();
+            TestTask2();
+        }
+
+        private static void TestTask()
+        {
+            Task<bool> t = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return true;
+            });
+            bool b = t.Result; // blocking on join here!
+            Console.WriteLine(b);
+            Console.Read();
+        }
+
+        private static async void TestTask2()
+        {
+            Console.WriteLine("Let's await!");
+            await GetTask(1000);
+            Console.WriteLine("Long last!");
+            Console.Read();
+        }
+
+        private static Task<int> GetTask(int wait)
+        {
+            var source = new TaskCompletionSource<int>();
+            //Task.Delay(100);
+            Thread.Sleep(wait);
+            source.SetResult(100);
+            return source.Task;
+        }
+    }
+}
---
+++ .NET/Certification/1. Multithreading/ThreadTests/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thr = new Thread(new ThreadStart(ThreadMethod));
+            thr.Start();
+            Thread.Sleep(120);
+            thr.Abort();
+            Console.Read();
+        }
+
+        private static void ThreadMethod()
+        {
+            try
+            {
+                foreach (var k in Enumerable.Range(0, 20))
+                {
+                    Thread.Sleep(20);
+                }
+            }
+            catch (ThreadAbortException tae)
+            {
+                Console.WriteLine("Thread aborted!");
+            }
+        }
+    }
+}
