f913356d2c4b4ef65447008e9cf1ab9f21914119
Shukanov <a.shukanov@itransition.com>
2016-08-31 18:07:08 +0300

    mvc study

---
+++ .NET/MVC/helloworld/hw/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace hw.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Name must be present!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email must be present!")]
+        [RegularExpression(".+\\@.+\\..+")] // does it contain dot and @?
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone must be present!")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you will attend!")]
+        public bool? WillAttend { get; set; }
+    }
+}
