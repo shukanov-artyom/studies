19a86868d42daaa6103e82ccc5d572cc2db74d12
a.shukanov <shukanov.artyom@gmail.com>
2016-09-11 22:29:48 +0300

    core experiments

---
+++ .NET/ASP.NET Core/Lectures/hw_/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using hw_.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace hw_.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var contact = new Contact()
+            {
+                FirstName = "Artyom",
+                LastName = "Shukanov"
+            };
+            return View(contact);
+        }
+    }
+}
---
+++ .NET/ASP.NET Core/Lectures/hw_/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace hw_.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
