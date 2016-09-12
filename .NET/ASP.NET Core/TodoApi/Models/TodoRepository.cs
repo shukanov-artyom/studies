96711d481418f1502bc5ce391a0b45fdb96632e5
Shukanov <a.shukanov@itransition.com>
2016-09-12 08:18:57 +0300

    dev

---
+++ .NET/ASP.NET Core/TodoApi/Models/TodoRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace TodoApi.Models
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private static ConcurrentDictionary<string, TodoItem> todos =
+              new ConcurrentDictionary<string, TodoItem>();
+
+        public TodoRepository()
+        {
+            Add(new TodoItem { Name = "Item1" });
+        }
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return todos.Values;
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            todos[item.Key] = item;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+            todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            todos[item.Key] = item;
+        }
+    }
+}
