1fa6cfebf27f4b878a0b47c79b4eabee4b666d26
a.shukanov <shukanov.artyom@gmail.com>
2016-09-20 21:42:36 +0300

    ASP.NET core development

---
+++ .NET/ASP.NET Core/livedemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+                
+            host.Run(); // запустили кестрел
+        }
+    }
+}
