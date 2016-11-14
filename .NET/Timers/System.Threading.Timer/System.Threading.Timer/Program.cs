dea1809863daaf301adfbba83f023a9085c9bb62
Artyom Shukanov <artyom_shukanov@epam.com>
2016-11-14 18:25:50 +0300

    Timers studies added

--- .NET/Timers/System.Threading.Timer/System.Threading.Timer/Program.cs
+++ .NET/Timers/System.Threading.Timer/System.Threading.Timer/Program.cs
@@ -8,7 +8,13 @@ namespace System.ThreadingTimer.Study
         public static void Main(string[] args)
         {
             // создаём потоковый таймер
-            TimerCallback cb = state => { Console.WriteLine("Timer called."); };
+            // делегат будет выполнен на потоке из пула!
+            TimerCallback cb = state =>
+            {
+                Console.WriteLine("Timer called.");
+                Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}.");
+                Console.WriteLine($"Is from pool: {Thread.CurrentThread.IsThreadPoolThread}.");
+            };
             Timer timer = new Timer(cb, null, 1000, 1000);
             // отдельно запускать таймер не требуется
             GC.Collect();
@@ -16,11 +22,30 @@ namespace System.ThreadingTimer.Study
 
             /* если собрать этот пример в релизе, то дебагер не будет держать 
             локальные ссылки до выхода из блока, и таймер не выполнится ни разу.*/
+
+            // второй пример - с юзингом таймер отрабатывает как надо
+            Main2();
         }
 
         /*
          * второй пример использует Using, и юзинг держит ссылку на таймер до конца, 
          * чтобы в конце вызвать Dispose()
          */
+
+        public static void Main2()
+        {
+            TimerCallback cb = state =>
+            {
+                Console.WriteLine("Timer called.");
+                Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}.");
+                Console.WriteLine($"Is from pool: {Thread.CurrentThread.IsThreadPoolThread}.");
+            };
+            using (Timer timer = new Timer(cb, null, 1000, 1000))
+            {
+                // отдельно запускать таймер не требуется
+                GC.Collect();
+                Thread.Sleep(10000);
+            }
+        }
     }
 }
