1297e33ea24561bbf2e8af1d22268ddd2f1abe00
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-12-18 11:35:12 +0300

    primitive Scraper sreated

--- Node.js/scraper/index.js
+++ Node.js/scraper/index.js
@@ -5,9 +5,32 @@ const { JSDOM } = jsdom;
 
 var url = 'https://stackoverflow.com/questions/7372972/how-do-i-parse-a-html-page-with-node-js';
 download(url).then(data => {
-    const dom = new JSDOM(data.toString());
-    const ps = dom.window.document.querySelectorAll('p');
-    for (var key in ps) {
-        console.log(key + ' : ' + ps[key]);
-    }
+    const scrapped = scrapHtml(data.toString());
+    console.log(scrapped);
 });
+
+function fits(text) {
+    var forbiddenWords = [ 'http' ];
+    for (var word in forbiddenWords) {
+        if (text.indexOf(word) !== -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function scrapHtml(html) {
+    const tags = ['p', 'h1', 'h2', 'h3'];
+    var result = "";
+    const dom = new JSDOM(html);
+    for (var tag in tags) {
+        const elements = dom.window.document.querySelectorAll(tags[tag]);
+        for (var key in elements) {
+            var text = elements[key].textContent;
+            if (text && fits(text)) {
+                result = result + '\r\n' + text;
+            }
+        }
+    }
+    return result;
+}
