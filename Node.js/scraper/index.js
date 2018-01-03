f49b5407f096cb4665767e86794a3538ac4c0ca2
Artyom Shukanov <shukanov.artyom@gmail.com>
2018-01-03 15:55:57 +0300

    studies

--- Node.js/scraper/index.js
+++ Node.js/scraper/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+
 const download = require('download');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
@@ -11,26 +11,27 @@ download(url).then(data => {
 
 function fits(text) {
     var forbiddenWords = [ 'http' ];
-    for (var word in forbiddenWords) {
-        if (text.indexOf(word) !== -1) {
-            return false;
-        }
-    }
+    forbiddenWords.forEach(function(word) {
+            if (text.indexOf(word) !== -1) {
+                return false;
+            }
+        });
     return true;
 }
 
 function scrapHtml(html) {
-    const tags = ['p', 'h1', 'h2', 'h3'];
+    const tagsToExtract = ['p', 'h1', 'h2', 'h3'];
     var result = "";
     const dom = new JSDOM(html);
-    for (var tag in tags) {
-        const elements = dom.window.document.querySelectorAll(tags[tag]);
+    tagsToExtract.forEach(function(tag){
+        const elements = 
+            dom.window.document.querySelectorAll(tag);
         for (var key in elements) {
             var text = elements[key].textContent;
             if (text && fits(text)) {
                 result = result + '\r\n' + text;
             }
         }
-    }
+    });
     return result;
 }
