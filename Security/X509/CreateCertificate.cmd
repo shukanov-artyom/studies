ddbff887b541a9082ad65a303204b5047f6591a5
Artyom Shukanov <shukanov.artyom@gmail.com>
2018-01-05 13:55:25 +0300

    Script modified

--- Security/X509/CreateCertificate.cmd
+++ Security/X509/CreateCertificate.cmd
@@ -3,4 +3,4 @@ REM Create certificate for specified certificate store
 makecert -r -pe -n "CN=SigningCertificate" -b 12/29/2017 -e 01/01/2049 -sky signature -a sha256 -len 2048 -ss my -sr LocalMachine
 REM BE sure to install required tools https://www.microsoft.com/en-us/download/confirmation.aspx?id=19801
 REM Grant Provate Key access for Network Service
-"c:\Program Files (x86)\Windows Resource Kits\Tools\winhttpcertcfg.exe" -g -c LOCAL_MACHINE\My -s OrthobulletsSigning -a "NETWORK SERVICE"
+"c:\Program Files (x86)\Windows Resource Kits\Tools\winhttpcertcfg.exe" -g -c LOCAL_MACHINE\My -s SigningCertificate -a "NETWORK SERVICE"
